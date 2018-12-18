be7cfca536b6fc00f8ed7de0ff5e5987d863e633
Bayram <Inan.Bayram@EXXETA.com>
2018-12-18 15:17:01 +0100

    Last Update added

--- src/app/kubernetesMonitor/domain/domain.component.ts
+++ src/app/kubernetesMonitor/domain/domain.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import {Component,OnInit,Input} from '@angular/core';
+import {DatePipe} from '@angular/common';
+import {registerLocaleData} from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-domain',
@@ -8,14 +11,31 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DomainComponent implements OnInit {
 
+  timeout_limit: number = 5; // Minutes
+
   @Input() domain: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    registerLocaleData(localeDe);
   }
 
   getClass() {
     return "domain-container background-" + this.domain.status;
+  }
+
+  getLastUpdate() {
+
+    var pipe = new DatePipe('de-DE');
+    var date = new Date(this.domain.timestamp);
+  
+    return pipe.transform(date, "yyyy-MM-dd-hh:mm:ss");
+  }
+
+  lastUpdateWarning() {
+    var date_overtime = this.domain.timestamp + (this.timeout_limit * 60000);
+
+    return (date_overtime <= moment.now()) ? true: false;
   }
 }
--- src/app/kubernetesMonitor/model/Domain.ts
+++ src/app/kubernetesMonitor/model/Domain.ts
@@ -3,6 +3,7 @@ export class Domain {
   constructor(
     public name: string,
     public url: string,
+    public timestamp: string,
     public stages: {
       name: string,
       stages: string[]
